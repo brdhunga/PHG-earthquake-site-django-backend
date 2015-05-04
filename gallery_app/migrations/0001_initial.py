285deae9678b20b602e53c6877d75418a76b3e86
bivu <brdhunga@gmail.com>
2015-05-04 10:44:52 +0545

    gallery app  Image model added

---
+++ gallery_app/migrations/0001_initial.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Image',
+            fields=[
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
+                ('url', models.URLField(blank=True, default='')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
--- gallery_app/models.py
+++ gallery_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_comptible
 
-# Create your models here.
+
+@python_2_unicode_comptible
+class Image(models.Model):
+    '''
+    A single Image
+    '''
+    url = models.URLField(blank=True, default='')
+    caption = models.CharField(max_length=500, blank=True, default='')
+
+    def __str__(self):
+        return self.caption
+
+
+
+
